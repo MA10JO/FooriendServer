70b9d9bb9af1d333d30b5a45cff19eaf06913b42
Jeong Hi <tresorhi@naver.com>
2022-11-21 00:26:15 +0900

    로그인

--- review/urls.py
+++ review/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [  # IP주소/blog/
+urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('create_post/', views.PostCreate.as_view()),
