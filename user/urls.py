b9ea956ac8cf179906e1fdfe53c784f62194a692
wi5h <114387225+wi5h@users.noreply.github.com>
2022-12-02 00:30:07 +0900

    user url 변경,

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SignUpView, SignInView
 
 
-#urlpatterns = [
+urlpatterns = [
     #path('signup/', SignUpView.as_view()),
     #path('signin/', SignInView.as_view()),
-#]
+]
